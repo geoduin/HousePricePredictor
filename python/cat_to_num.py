6818fb75249f7bc0980ae770440c387459e14fc5
Xin Wang <Xin20Wang@outlook.com>
2024-10-29 13:49:01 +0100

    Pushed new data

---
+++ python/cat_to_num.py
@@ -0,0 +1,6 @@
+from sklearn.preprocessing import OrdinalEncoder
+from pandas import DataFrame
+
+def convert_cat_to_numeric(data: DataFrame, mapping: list, column_name: str):
+    
+    return data

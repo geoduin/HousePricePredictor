5e4a620ea5cbb7f35708754ac1379435392806ec
Xin Wang <Xin20Wang@outlook.com>
2024-10-30 10:04:07 +0100

    dfs

---
+++ .history/python_lib/data_preperation_20241030100401.py
@@ -0,0 +1,28 @@
+
+def impute_empty_category_data(df):
+
+    # Missende waarden opvullen met NA waarde.
+    df["FireplaceQu"] = df["FireplaceQu"].fillna("NA")
+    df["GarageFinish"] = df["GarageFinish"].fillna("NA")
+    
+
+    
+    return df
+
+def remove_outliers(df):
+    # Outliers verwijderen, mits het aantal niet te groot is.
+    filtering_grlivArea = df["GrLivArea"] < 4000
+    df["GrLivArea"] = df[filtering_grlivArea]
+
+    print(len(df))
+    # filtering_overqual = df["OverallQual"] > 1.5
+    # df["OverallQual"] = df[filtering_overqual]
+
+    # print(len(df))
+    # filtering_totRms = df["TotRmsAbvGrd"] < 11
+    # df["TotRmsAbvGrd"] = df[filtering_totRms]
+    
+    return df
+
+def _is_dtype(column):
+    return column.dtype == "O"
